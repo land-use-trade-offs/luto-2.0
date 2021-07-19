6fcb197aae3e97fbbfad518234572168cc32fca5
Fjalar <fjalar@fjalar.org>
2021-07-19 18:35:29 +1000

    Found error, it seems.

--- luto/economics/transitions.py
+++ luto/economics/transitions.py
@@ -68,17 +68,27 @@ def get_transition_matrix(year, lumap, lmmap):
 
     # Switching to irr, from and to an irr land-use, may incur licence costs.
     tdelta_toirr_rj = np.zeros((ncells, nlus))
-    for r in range(ncells):
-        lu = lumap[r]
-        lm = lmmap[r]
-        if lm == 1: # If it is currently an irrigated land use.
-            # Difference with current licence is paid or refunded.
-            tdelta_toirr_rj[r] = aqlic_rj[r] - aqlic_rj[r, lu]
-        else: # If it is currently a non-irrigated land use.
-            # Licence cost and infrastructure costs @ 10kAUD/ha.
-            tdelta_toirr_rj[r] = aqlic_rj[r] + (10**4 * realarea_rj[r])
 
-    # Switching to dry, from and to an irr land-use, may incurlicence refund.
+    # IRR -> IRR / Difference with current licence paid or refunded.
+    aqlic_lu = np.stack(tuple(aqlic_rj[r, lumap[r]] for r in range(len(lumap))))
+    aqlic_lurj = np.stack((aqlic_lu,) * nlus, axis=1)
+    irr = lmmap==1
+    tdelta_toirr_rj[irr] = aqlic_rj[irr] - aqlic_lurj[irr]
+    # DRY -> IRR / Licence costs plus infrastructure costs.
+    dry = lmmap==0
+    tdelta_toirr_rj[dry] = aqlic_rj[dry] + 10000 * realarea_rj[dry]
+
+    # for r in range(ncells):
+        # lu = lumap[r]
+        # lm = lmmap[r]
+        # if lm == 1: # If it is currently an irrigated land use.
+            # # Difference with current licence is paid or refunded.
+            # tdelta_toirr_rj[r] = aqlic_rj[r] - aqlic_rj[r, lu]
+        # else: # If it is currently a non-irrigated land use.
+            # # Licence cost and infrastructure costs @ 10kAUD/ha.
+            # tdelta_toirr_rj[r] = aqlic_rj[r] + (10**4 * realarea_rj[r])
+
+    # Switching to dry, from and to an irr land-use, may incur licence refund.
     tdelta_todry_rj = np.zeros((ncells, nlus))
     for r in range(ncells):
         lu = lumap[r]
@@ -99,9 +109,17 @@ def get_transition_matrix_old(year, lumap):
     # Raw transition-cost matrix is in AUD/Ha. Set NaNs to zero.
     t_ij = np.nan_to_num(data.TCOSTMATRIX)
 
+
     # Infer number land-uses and cells from t_ij and lumap matrices.
     nlus = t_ij.shape[0]
     ncells = lumap.shape[0]
+
+    t_ij = np.zeros_like(t_ij)
+    t_ij[ ::2,  ::2] = data.TMATRIX.to_numpy()
+    t_ij[ ::2, 1::2] = data.TMATRIX.to_numpy()
+    t_ij[1::2,  ::2] = data.TMATRIX.to_numpy()
+    t_ij[1::2, 1::2] = data.TMATRIX.to_numpy()
+
 
     # Transition costs to commodity j at cell r using present lumap (in AUD/ha).
     t_rj_audperha = np.stack(tuple(t_ij[lumap[r]] for r in range(ncells)))
@@ -127,11 +145,37 @@ def get_transition_matrix_old(year, lumap):
                                    # , aqlic_rj[r] - aqlic_rj[r, j] )
         else: # If it is currently a non-irrigated land use.
             tdelta_rj[r] = aqlic_rj[r]
+            # tdelta_rj[r, 1::2] += 10**4 * realarea_rj[r, 1::2]
 
     # Transition costs to commodity j at cell r converted to AUD per cell.
-    t_rj = t_rj_audperha * realarea_rj + tdelta_rj
+    delta = np.zeros_like(realarea_rj)
+    delta[:, 1::2] = 10000 # * realarea_rj[:, 1::2]
+    delta[lumap%2==1] = 0
+
+    brackets = (1 + delta) * realarea_rj
+    expanded = realarea_rj + delta*realarea_rj
+
+    t_rj = (t_rj_audperha + delta) * realarea_rj + tdelta_rj
 
     # Amortise the transition costs.
     t_rj = amortise(t_rj)
 
-    return t_rj
+    return t_rj, brackets, expanded, delta, realarea_rj
+
+if __name__ == '__main__':
+    import luto.data as data
+    from luto.economics.transitions import *
+
+    lumap = np.load('input/lumap.npy')
+    lmmap = np.load('input/lmmap.npy')
+    lumapold = np.load('input/lumap-dryirr.npy')
+
+    t_rj, brackets, expanded, delta, realarea_rj = get_transition_matrix_old(0, lumapold)
+    t_rj_todry, t_rj_toirr = get_transition_matrix(0, lumap, lmmap)
+
+    rabrick = np.stack((data.REAL_AREA,) * 23, axis=1)
+    rawbrick = np.stack((data.REAL_AREA,) * 46, axis=1)
+
+    d = (t_rj[:, 1::2] - t_rj_toirr)
+
+    print(d, (d == 0).all())
