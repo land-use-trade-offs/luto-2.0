ddcec28925e9c6e797f68ce465e4fb5482c0614d
Fjalar <fjalar@fjalar.org>
2021-07-30 16:42:45 +1000

    Tests interpreted as new solver structure okay. Transition completed. In progress: transition to new data set.

--- luto/solvers/solver.py
+++ luto/solvers/solver.py
@@ -4,7 +4,7 @@
 #
 # Author: Fjalar de Haan (f.dehaan@deakin.edu.au)
 # Created: 2021-02-22
-# Last modified: 2021-07-29
+# Last modified: 2021-07-30
 #
 
 import numpy as np
@@ -19,40 +19,6 @@ constraints = { 'water': True
               , 'biodiversity': True
               }
 
-def slyce(a, axis, start, stop, step):
-    """Return a view of the slice along the requested axis.
-
-    Using trick from Leland Hepworth on Stack Overflow as found here:
-    [ https://stackoverflow.com/questions/24398708/
-      slicing-a-numpy-array-along-a-dynamically-specified-axis ]
-    """
-    return a[(slice(None),) * (axis % a.ndim) + (slice(start, stop, step),)]
-
-def coursify(array, resfactor):
-    """Return course-grained version of array, coursened by `resfactor`."""
-    # Every index * `resfactor` is sampled. New array length varies.
-    return array[::resfactor]
-
-def uncoursify(array, resfactor, presize=None):
-    """Return array inflated by `resfactor`. Inverse of `coursify()`."""
-
-    # Determine the shape of the output array as well as the dtype.
-    if presize is None: presize = resfactor * array.shape[0]
-    bshape = list(array.shape)
-    bshape[0] = presize
-    bshape = tuple(bshape)
-    brray = np.ones(bshape, dtype=array.dtype)
-
-    # Output will be length presize. Second for-loop to fill out tail end.
-    for i in range(0, len(brray) - resfactor, resfactor):
-        for k in range(resfactor):
-            brray[i+k] = array[i // resfactor]
-    i += resfactor
-    for k in range(presize - (array.shape[0]-1)*resfactor):
-        brray[i+k] = array[i // resfactor]
-
-    return brray
-
 def solve( t_mrj  # Transition cost matrices.
          , c_mrj  # Production cost matrices.
          , q_mrj  # Yield matrices.
@@ -61,7 +27,15 @@ def solve( t_mrj  # Transition cost matrices.
          , x_mrj  # Exclude matrices.
          , constraints = constraints # Constraints to use (default all).
          ):
-    """Return land-use, land-man maps under constraints and minimised costs."""
+    """Return land-use, land-man maps under constraints and minimised costs.
+
+    All inputs are Numpy arrays of the appropriate shapes, except for `p` which
+    is a scalar and `constraints` which is a dictionary.
+
+    To run with only a subset of the constraints, pass a custom `constraints`
+    dictionary. Format {key: value} where 'key' is a string, one of 'water',
+    'nutrients', 'carbon' or 'biodiversity' and 'value' is either True or False.
+    """
 
     # Extract the shape of the problem (# land-man types, cells and land-uses).
     nlms, ncells, nlus = t_mrj.shape
@@ -118,6 +92,19 @@ def solve( t_mrj  # Transition cost matrices.
         # Magic.
         model.optimize()
 
+        # Water use capped, per catchment, at volume consumed in base year.
+        if constraints['water']:
+            ...
+
+        if constraints['nutrients']:
+            ...
+
+        if constraints['carbon']:
+            ...
+
+        if constraints['biodiversity']:
+            ...
+
         # Collect optimised decision variables in tuple of 1D Numpy arrays.
         prestack_dry = tuple(X_dry[j].X for j in range(nlus))
         stack_dry = np.stack(prestack_dry)
@@ -136,120 +123,8 @@ def solve( t_mrj  # Transition cost matrices.
 
         return lumap, lmmap
 
-
-
-
     except gp.GurobiError as e:
         print('Gurobi error code', str(e.errno), ':', str(e))
 
     except AttributeError:
         print('Encountered an attribute error')
-
-
-
-def solve_old( t_rj  # Transition cost to commodity j at cell r - w lumap info.
-             , c_rj  # Cost of producing commodity j at cell r.
-             , q_rj  # Yield of commodity j at cell r.
-             , d_j   # Demand for commodity j.
-             , p     # Penalty level. Multiplies cost/unit surplus or deficits.
-             , x_rj  # Possible/allowed land uses j at cell r.
-             , constraints = constraints # Constraints to use (default all).
-             ):
-    """Return new lu map. Least-cost, surplus/deficit penalties as costs.
-
-    Note that the t_rj transition-cost matrix _includes_ the information of the
-    current land-use map, which therefore is not passed seperately.
-
-    All inputs are Numpy arrays of the appropriate shapes, except for p which
-    is a scalar multiplier.
-
-    To run with only a subset of the constraints, pass a custom `constraints`
-    dictionary. Format {key: value} where 'key' is a string, one of 'water',
-    'nutrients', 'carbon' or 'biodiversity' and 'value' is either True or False.
-    """
-
-    # Extract the shape of the problem.
-    ncells = t_rj.shape[0]
-    nlus = t_rj.shape[1]
-
-    # Calculate the penalty units for each land use j as its maximum cost.
-    p_j = np.zeros(nlus // 2)
-    for j in range(nlus // 2):
-        k = 2 * j
-        p_j[j] = max( c_rj.T[k].max()
-                    , c_rj.T[k+1].max() )
-
-    # Apply the multiplier.
-    p_j *= p
-
-    try:
-        # Make Gurobi model instance.
-        model = gp.Model('neoLUTO v0.03')
-        # model.Params.Method = 1    # Use dual simplex only. Save memory.
-
-        # Land-use indexed list of ncells-sized decision variable vectors.
-        X = [model.addMVar(ncells, ub=x_rj.T[j], name='X') for j in range(nlus)]
-
-        # Decision variables to minimise the deviations.
-        V = model.addMVar(nlus // 2, name='V')
-
-        # Set the objective function and the model sense.
-        objective = ( sum( c_rj.T[j] @ X[j]              # Cost of producing.
-                         + t_rj.T[j] @ X[j]              # Cost of switching.
-                           for j in range(nlus) )        # For each land use.
-                    + sum( V[k]
-                           for k in range(nlus // 2) ) # Cost of surpl/defct.
-                    )
-        model.setObjective(objective, GRB.MINIMIZE)
-
-        # Constraint that all of every cell is used for some land use.
-        model.addConstr(sum(X) == np.ones(ncells))
-
-        # Constraints that penalise deficits and surpluses, respectively.
-        # Demands per actual land-use, irrespective of irrigation status.
-        model.addConstrs( p_j[j]
-                        * ( d_j[j] - ( q_rj.T[2*j]   @ X[2*j]
-                                     + q_rj.T[2*j+1] @ X[2*j+1] ) ) <= V[j]
-                          for j in range(nlus // 2) )
-        model.addConstrs( p_j[j]
-                        * ( ( q_rj.T[2*j]   @ X[2*j]
-                            + q_rj.T[2*j+1] @ X[2*j+1] ) - d_j[j] ) <= V[j]
-                          for j in range(nlus // 2) )
-
-        # Only add the following constraints if requested.
-
-        # Water use capped, per catchment, at volume consumed in base year.
-        if constraints['water']:
-            ...
-            # # For each catchment
-            # for c, cap in watercaps:
-                # model.addConstrs( w_rj.T[2*j+1] @ X[2*j+1] <= cap
-                                  # for j in range(nlus // 2) )
-
-        if constraints['nutrients']:
-            ...
-
-        if constraints['carbon']:
-            ...
-
-        if constraints['biodiversity']:
-            ...
-
-        # Magic.
-        model.optimize()
-
-        # Collect optimised decision variables in tuple of 1D Numpy arrays.
-        prestack = tuple(X[j].X for j in range(nlus))
-
-        # Flatten into land-use map of the usual highpos integer array format.
-        highpos = np.stack(prestack).argmax(axis=0)
-
-        # Return the land-use map, i.e. the highpos array.
-        return highpos #, np.stack(prestack)
-
-    except gp.GurobiError as e:
-        print('Gurobi error code', str(e.errno), ':', str(e))
-
-    except AttributeError:
-        print('Encountered an attribute error')
-
--- run-neoluto.py
+++ run-neoluto.py
@@ -3,8 +3,8 @@
 # run-neoluto.py - to run neoLUTO.
 #
 # Author: Fjalar de Haan (f.dehaan@deakin.edu.au)
-# Created: 2021-04-28
-# Last modified: 2021-07-28
+# Created: 2021-07-28
+# Last modified: 2021-07-30
 #
 
 import os.path
@@ -14,113 +14,97 @@ import numpy as np
 # Load data module and initialise with ANO Scenario 236.
 import luto.data as data
 
-from luto.economics.cost import get_cost_matrix_old
-from luto.economics.quantity import get_quantity_matrix_old
-from luto.economics.transitions import get_transition_matrix_old
+from luto.economics.cost import get_cost_matrices
+from luto.economics.quantity import get_quantity_matrices
+from luto.economics.transitions import get_transition_matrices
 
-from luto.solvers.solver import coursify, uncoursify
-from luto.solvers.solver import solve_old as solve
+from luto.solvers.resfactor import coursify, uncoursify
+from luto.solvers.solver import solve
 
 from luto.tools import timethis, inspect, ctabnpystocsv
 from luto.tools.highposgtiff import write_highpos_gtiff
 
-# Required input data - for now, just year zero.
-#
+# Required input data.
 year = 0
+lumap = np.load(os.path.join(data.INPUT_DIR, 'lumap.npy'))
+lmmap = np.load(os.path.join(data.INPUT_DIR, 'lmmap.npy'))
+t_mrj = get_transition_matrices(year, lumap, lmmap)
+c_mrj = get_cost_matrices(year)
+q_mrj = get_quantity_matrices(year)
 
-# Present land-use map in highpos format. Based on oldLUTO's SID array.
-lumap = np.load(os.path.join(data.INPUT_DIR, 'lumap-dryirr.npy'))
+nlms, ncells, nlus = t_mrj.shape
 
-# Transition cost to commodity j at cell r.
-t_rj = get_transition_matrix_old(year, lumap)
-
-# Cost of producing commodity j at cell r.
-c_rj = get_cost_matrix_old(year)
-
-# Yield of commodity j at cell r.
-q_rj = get_quantity_matrix_old(year)
-
-# Demand and penalty for surplus/deficit for commodity j.
-#
-# As a first-order, semi-realistic, base-line demand, use current production.
-d_j = np.zeros(data.NLUS // 2) # Prepare the demands array - one cell per LU.
-
-for j in range(data.NLUS // 2):
-    k = 2 * j
+d_j = np.zeros(nlus) # Prepare the demands array - one cell per LU.
+for j in range(nlus):
     # The demand for LU j is the dot product of the yield vector with a
     # summation vector indicating where LU actually occurs as per SID array.
-    #        [ yield of j for all r ] . [ all r where j occurs ]
-    d_j[j] = ( q_rj.T[k]   @ np.where(lumap == k, 1, 0)
-             + q_rj.T[k+1] @ np.where(lumap == k+1, 1, 0) )
+    d_j[j] = ( q_mrj[0].T[j] @ np.where((lumap==j) & (lmmap==0), 1, 0)
+             + q_mrj[1].T[j] @ np.where((lumap==j) & (lmmap==1), 1, 0) )
 
 # Default penalty level.
 p = 1
 
 # Possible land uses j at cell r.
-x_rj = data.x_rj
+x_mrj = data.x_mrj
 
-params_data = { "t_rj" : t_rj
-              , "c_rj" : c_rj
-              , "q_rj" : q_rj
-              , "d_j"  : d_j
-              , "p"    : p
-              , "x_rj" : x_rj
+params_data = { "t_mrj" : t_mrj
+              , "c_mrj" : c_mrj
+              , "q_mrj" : q_mrj
+              , "d_j"   : d_j
+              , "p"     : p
+              , "x_mrj" : x_mrj
               }
 
-def randomparams(ncells, nlus, p=1):
-    # `nlus` needs to be an even number because land-uses come as dry/irr pairs.
-    if nlus % 2 == 1:
-        raise ValueError("nlus needs to be an even integer")
-    else:
-        # Bogus lumap.
-        lumap = np.random.randint(nlus, size=ncells)
+def randomparams(nlms, ncells, nlus, p=1):
+    # Bogus lumap.
+    lumap = np.random.randint(nlus, size=ncells)
 
-        # Transition cost matrix.
-        t_ij = 10 * np.random.random((nlus, nlus))
-        for i in range(nlus): t_ij[i, i] = 0
+    # Transition cost matrix.
+    t_ij = 10 * np.random.random((nlus, nlus))
+    for i in range(nlus): t_ij[i, i] = 0
+    t_rj = np.stack(tuple(t_ij[lumap[r]] for r in range(ncells)))
+    t_mrj = np.stack((t_rj, 1.5*t_rj))
 
-        t_rj = np.stack(tuple(t_ij[lumap[r]] for r in range(ncells)))
+    # Production cost matrix.
+    c_mrj = 10 * np.random.random((nlms, ncells, nlus))
 
-        # Production cost matrix.
-        c_rj = 10 * np.random.random((ncells, nlus))
+    # Yield matrix.
+    q_mrj = 10 * np.random.random((nlms, ncells, nlus))
 
-        # Yield matrix.
-        q_rj = 10 * np.random.random((ncells, nlus))
+    # Demands.
+    d_j = 10 * np.random.random(nlus)
 
-        # Demands.
-        d_j = 10 * np.random.random(nlus)
+    # Exclude matrix.
+    x_mrj = np.ones_like(t_mrj)
 
-        # Exclude matrix.
-        x_rj = np.ones_like(t_rj)
-
-        return { "t_rj" : t_rj
-               , "c_rj" : c_rj
-               , "q_rj" : q_rj
-               , "d_j"  : d_j
-               , "p"    : p
-               , "x_rj" : x_rj
-               }
+    return { "t_mrj" : t_mrj
+           , "c_mrj" : c_mrj
+           , "q_mrj" : q_mrj
+           , "d_j"  : d_j
+           , "p"     : p
+           , "x_mrj" : x_mrj
+           }
 
 def run_params(params, resfactor=1):
 
     if resfactor == 1:
-        highpos = timethis( solve
-                        , params["t_rj"]
-                        , params["c_rj"]
-                        , params["q_rj"]
-                        , params["d_j"]
-                        , params["p"]
-                        , params["x_rj"] )
+        lumap, lmmap = timethis( solve
+                               , params["t_mrj"]
+                               , params["c_mrj"]
+                               , params["q_mrj"]
+                               , params["d_j"]
+                               , params["p"]
+                               , params["x_mrj"] )
     else:
-        ncells, nlus = params["t_rj"].shape
+        nlms, ncells, nlus = params["t_mrj"].shape
 
         print("Applying resfactor =", str(resfactor))
 
         print("\t" + "Course-graining input arrays...")
-        t_rj = coursify(params["t_rj"], resfactor)
-        c_rj = coursify(params["c_rj"], resfactor)
-        q_rj = coursify(params["q_rj"], resfactor)
-        x_rj = coursify(params["x_rj"], resfactor)
+        t_mrj = coursify(params["t_mrj"], resfactor)
+        c_mrj = coursify(params["c_mrj"], resfactor)
+        q_mrj = coursify(params["q_mrj"], resfactor)
+        x_mrj = coursify(params["x_mrj"], resfactor)
 
         print("\t" + "Adjusting demands...")
         d_j = params["d_j"] / resfactor
@@ -129,156 +113,14 @@ def run_params(params, resfactor=1):
         p = params["p"] * resfactor
 
         highpos = timethis( solve
-                          , t_rj
-                          , c_rj
-                          , q_rj
+                          , t_mrj
+                          , c_mrj
+                          , q_mrj
                           , d_j
                           , p
-                          , x_rj )
+                          , x_mrj )
 
         print("Inflating highpos output array to original original extent...")
         highpos = uncoursify(highpos, resfactor, presize=ncells)
 
-    return highpos
-
-def run(p=1, write=True):
-    highpos = timethis( solve
-                      , t_rj
-                      , c_rj
-                      , q_rj
-                      , d_j
-                      , p
-                      , x_rj )
-
-    df = inspect(lumap, highpos, d_j, q_rj, c_rj, data.LANDUSES)
-
-    print(df)
-
-    if write:
-        np.save('highpos.npy', highpos)
-        df.to_csv('diff.csv')
-        write_highpos_gtiff('highpos.npy', 'highpos')
-        ctabnpystocsv( os.path.join(data.INPUT_DIR, 'lumap.npy')
-                     , 'highpos.npy'
-                     , data.LANDUSES )
-
-
-    return df, highpos
-
-def run_decigoogol_penalty():
-    highpos = timethis( solve
-                      , t_rj
-                      , c_rj
-                      , q_rj
-                      , d_j
-                      , 10**10
-                      , x_rj )
-
-    df = inspect(lumap, highpos, d_j, q_rj, c_rj, data.LANDUSES)
-
-    print(df)
-
-    return df, highpos
-
-def run_random(ncells, nlus, p=1, resfactor=1):
-    """Run model with random data, penalty-level `p` and shape ncells, nlus."""
-
-    # `nlus` needs to be an even number because land-uses come as dry/irr pairs.
-    if nlus % 2 == 1:
-        raise ValueError("nlus needs to be an even integer")
-    else:
-        # Bogus lumap.
-        lumap = np.random.randint(nlus, size=ncells)
-
-        # Transition cost matrix.
-        t_ij = 10 * np.random.random((nlus, nlus))
-        for i in range(nlus): t_ij[i, i] = 0
-
-        t_rj = np.stack(tuple(t_ij[lumap[r]] for r in range(ncells)))
-
-        # Production cost matrix.
-        c_rj = 10 * np.random.random((ncells, nlus))
-
-        # Yield matrix.
-        q_rj = 10 * np.random.random((ncells, nlus))
-
-        # Demands.
-        d_j = 10 * np.random.random(nlus)
-
-        # Exclude matrix.
-        x_rj = np.ones_like(t_rj) # np.random.randint(2, size=(ncells, nlus), dtype=np.int8)
-
-        if resfactor == 1:
-            highpos = timethis( solve
-                            , t_rj
-                            , c_rj
-                            , q_rj
-                            , d_j
-                            , p
-                            , x_rj )
-        else:
-            print("Applying resfactor =", str(resfactor))
-
-            print("\t" + "Course-graining input arrays...")
-            t_rj = coursify(t_rj, resfactor)
-            c_rj = coursify(c_rj, resfactor)
-            q_rj = coursify(q_rj, resfactor)
-            x_rj = coursify(x_rj, resfactor)
-
-            print("\t" + "Adjusting demands...")
-            d_j /= resfactor
-
-            print("\t" + "Adjusting penalty level...")
-            p *= resfactor
-
-            highpos = timethis( solve
-                            , t_rj
-                            , c_rj
-                            , q_rj
-                            , d_j
-                            , p
-                            , x_rj )
-            print("Inflating highpos output array to original original extent...")
-            highpos = uncoursify(highpos, resfactor, presize=ncells)
-
-    return highpos
-
-def runstack_random(ncells, nlus, p=1):
-    """Run stacked model w/ random data, penalty-level `p` and shape ncells, nlus."""
-
-    # Bogus lumap.
-    lumap = np.random.randint(nlus, size=ncells)
-
-    # Transition cost matrix.
-    t_ij = 10 * np.random.random((nlus, nlus))
-    for i in range(nlus): t_ij[i, i] = 0
-
-    t_rj = np.stack(tuple(t_ij[lumap[r]] for r in range(ncells)))
-    tm_rj = 0.1 * t_rj
-
-    # Production cost matrix.
-    c_rj = 10 * np.random.random((ncells, nlus))
-    cm_rj = 0.1 * c_rj
-
-    # Yield matrix.
-    q_rj = 10 * np.random.random((ncells, nlus))
-    qm_rj = 5 * q_rj
-
-    # Demands.
-    d_j = 20 * np.random.random(nlus)
-
-    # Exclude matrix.
-    x_rj = np.ones((ncells, nlus)) # np.random.randint(2, size=(ncells, nlus), dtype=np.int8)
-
-    highpos, mngmnt = timethis( stacksolve
-                              , t_rj
-                              , tm_rj
-                              , c_rj
-                              , cm_rj
-                              , q_rj
-                              , qm_rj
-                              , d_j
-                              , p
-                              , x_rj )
-
-    return highpos, mngmnt
+    return lumap, lmmap
