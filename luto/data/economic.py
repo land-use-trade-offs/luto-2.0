ae7ecaabafe95ac7fed24f29f2b28738f1430ded
Fjalar <fjalar@fjalar.org>
2021-08-12 17:43:47 +1000

    Rewrote concord() to build_agec_crops(). Runtime now down to <1 minute from 4+ hours.

--- luto/data/economic.py
+++ luto/data/economic.py
@@ -4,13 +4,49 @@
 #
 # Author: Fjalar de Haan (f.dehaan@deakin.edu.au)
 # Created: 2021-07-09
-# Last modified: 2021-08-04
+# Last modified: 2021-08-12
 #
 
 import os.path
 
 import pandas as pd
 import numpy as np
+
+def build_agec_crops(concordance, data, columns=None, lus=None, lms=None):
+    """Return LUTO-cell wise, multi-indexed column-landuse-landman DataFrame."""
+
+    # If no list of columns is provided, infer it from provided data.
+    if columns is None:
+        columns = data.columns.to_list()
+
+    # If no list of land uses provided, infer it from the SPREAD_CLASS column.
+    if lus is None:
+        lus = sorted(data['LU_DESC'].unique().tolist())
+
+    # If no list of land managements provided, use irrigation status only.
+    if lms is None:
+        lms = ['dry', 'irr']
+
+    # Produce a multi-indexed version data and merge to yield cell-based table.
+    crops_ptable = data.pivot_table( index='SA2_ID'
+                                   , columns=['Irrigation', 'LU_DESC'] )
+    agec_crops = concordance.merge( crops_ptable
+                                  , left_on='SA2_ID'
+                                  , right_on=crops_ptable.index
+                                  , how='left' )
+
+    # Some columns have to go.
+    agec_crops = agec_crops.drop(['CELL_ID', 'SA2_ID'], axis=1)
+    columns = [ col for col in columns
+                if col != 'SA2_ID'
+                if col != 'LU_DESC'
+                if col != 'Irrigation' ]
+
+    # The merge flattens the multi-index to tuples, so unflatten here.
+    ts = [(t[0], lms[t[1]], t[2]) for t in agec_crops.columns]
+    agec_crops.columns = pd.MultiIndex.from_tuples(ts)
+
+    return agec_crops
 
 def concord(concordance, data, columns=None, lus=None, lms=None):
     """Return LUTO-cell wise, multi-indexed column-landuse-landman DataFrame."""
@@ -21,7 +57,7 @@ def concord(concordance, data, columns=None, lus=None, lms=None):
 
     # If no list of land uses provided, infer it from the SPREAD_CLASS column.
     if lus is None:
-        lus = sorted(data['SPREAD_CLASS'].unique().tolist())
+        lus = sorted(data['LU_DESC'].unique().tolist())
 
     # If no list of land managements provided, use irrigation status only.
     if lms is None:
@@ -29,9 +65,9 @@ def concord(concordance, data, columns=None, lus=None, lms=None):
 
     # Prepare column-wise multi-indexed pandas.DataFrame.
     mcolindex = pd.MultiIndex.from_product( [columns, lms, lus]
-                                           , names = [ 'DATUM'
-                                                     , 'LANDMANS'
-                                                     , 'LANDUSE' ] )
+                                          , names = [ 'DATUM'
+                                                    , 'LANDMANS'
+                                                    , 'LANDUSE' ] )
     index = range(concordance.shape[0])
     df = pd.DataFrame(columns=mcolindex, index=index)
 
