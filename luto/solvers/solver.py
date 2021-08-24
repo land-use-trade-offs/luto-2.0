6dfb92c172902a059af842cad19852cedc80f93d
Fjalar <fjalar@fjalar.org>
2021-08-24 17:23:59 +1000

    Solver using transformations. Behaviour seemingly correct. Dependence on penalties unintuitive.

--- luto/solvers/solver.py
+++ luto/solvers/solver.py
@@ -4,11 +4,12 @@
 #
 # Author: Fjalar de Haan (f.dehaan@deakin.edu.au)
 # Created: 2021-02-22
-# Last modified: 2021-08-23
+# Last modified: 2021-08-24
 #
 
 import numpy as np
 import pandas as pd
+import scipy.sparse as sp
 import gurobipy as gp
 from gurobipy import GRB
 
@@ -42,18 +43,22 @@ def solve( t_mrj  # Transition cost matrices.
     # Extract the shape of the problem.
     nlms, ncells, nlus = t_mrj.shape # Number of landmans, cells, landuses.
     _, _, nprs = q_mrp.shape # Number of products.
-    ncms = d_c.shape # Number of commodities.
+    ncms, = d_c.shape # Number of commodities.
 
     # Penalty units for each j as maximum cost.
     p_c = np.zeros(ncms)
     for c in range(ncms):
-        p_c[j] = c_mrj.T.max()
+        p_c[c] = c_mrj.T.max()
     # Apply the penalty-level multiplier.
     p_c *= p
 
     try:
         # Make Gurobi model instance.
         model = gp.Model('neoLUTO v0.1.0')
+
+        # ------------------- #
+        # Decision variables. #
+        # ------------------- #
 
         # Land-use indexed lists of ncells-sized decision variable vectors.
         X_dry = [ model.addMVar(ncells, ub=x_mrj[0, :, j], name='X_dry')
@@ -64,6 +69,10 @@ def solve( t_mrj  # Transition cost matrices.
         # Decision variables to minimise the deviations.
         V = model.addMVar(ncms, name='V')
 
+        # ------------------- #
+        # Objective function. #
+        # ------------------- #
+
         # Set the objective function and the model sense.
         objective = ( sum( # Production costs.
                            c_mrj[0].T[j] @ X_dry[j]
@@ -71,11 +80,16 @@ def solve( t_mrj  # Transition cost matrices.
                            # Transition costs.
                          + t_mrj[0].T[j] @ X_dry[j]
                          + t_mrj[1].T[j] @ X_irr[j]
-                           # Penalties.
-                           + V[j] # TODO: Runs over c instead of j.
                            # For all land uses.
-                           for j in range(nlus) ) )
+                           for j in range(nlus) )
+                    + sum( # Penalties.
+                           V[c] for c in range(ncms) )
+                    )
         model.setObjective(objective, GRB.MINIMIZE)
+
+        # ------------ #
+        # Constraints. #
+        # ------------ #
 
         # Constraint that all of every cell is used for some land use.
         model.addConstr( sum( X_dry
@@ -84,46 +98,32 @@ def solve( t_mrj  # Transition cost matrices.
         # Constraints that penalise deficits and surpluses, respectively.
         #
 
-        # Awkward matrix multiplication to get around gp.MLinExpr limitations.
-        # X_dry_pr = [ sum(lu2pr_pj.T[j, p] * X_dry[j] for j in range(nlus))
-                     # for p in range(nprs) ]
-        # X_irr_pr = [ sum(lu2pr_pj.T[j, p] * X_irr[j] for j in range(nlus))
-                     # for p in range(nprs) ]
-
+        # Transform decision vars from LU/j to PR/p representation.
         X_dry_pr = [ X_dry[j] for p in range(nprs) for j in range(nlus)
                      if lu2pr_pj[p, j] ]
         X_irr_pr = [ X_irr[j] for p in range(nprs) for j in range(nlus)
                      if lu2pr_pj[p, j] ]
 
-        # Gurobi official workaround:
-        q_dry_p = [ sp.diags(q_mrp[0].T[p]) @ X_dry_pr[p]
-                    for p in range(nprs) ]
-        q_irr_p = [ sp.diags(q_mrp[0].T[p]) @ X_irr_pr[p]
-                    for p in range(nprs) ]
-        q_dry_c = [ q_dry_p[p] for c in range(ncms) for p in range(nprs)
-                    if pr2cm_cp[c, p] ]
-        q_irr_c = [ q_irr_p[p] for c in range(ncms) for p in range(nprs)
-                    if pr2cm_cp[c, p] ]
+        # Quantities in PR/p representation by land-management (dry/irr).
+        q_dry_p = [ q_mrp[0].T[p] @ X_dry_pr[p] for p in range(nprs) ]
+        q_irr_p = [ q_mrp[0].T[p] @ X_irr_pr[p] for p in range(nprs) ]
 
+        # Transform quantities to CM/c representation by land-man (dry/irr).
+        q_dry_c = [ sum(q_dry_p[p] for p in range(nprs) if pr2cm_cp[c, p])
+                    for c in range(ncms) ]
+        q_irr_c = [ sum(q_irr_p[p] for p in range(nprs) if pr2cm_cp[c, p])
+                    for c in range(ncms) ]
+
+        # Total quantities in CM/c representation.
+        q_c = [q_dry_c[c] + q_irr_c[c] for c in range(ncms)]
+
+        # Finally, add the constraint in the CM/c representation.
         model.addConstrs( p_c[c] * (d_c[c] - q_c[c]) <= V[c]
                           for c in range(ncms) )
         model.addConstrs( p_c[c] * (q_c[c] - d_c[c]) <= V[c]
                           for c in range(ncms) )
 
-        # Non landuse-product-commodity version. For reference:
-        # model.addConstrs( p_j[j]
-                        # * ( d_j[j] - ( q_mrj[0].T[j] @ X_dry[j]
-                                     # + q_mrj[1].T[j] @ X_irr[j] ) ) <= V[j]
-                          # for j in range(nlus) )
-        # model.addConstrs( p_j[j]
-                        # * ( ( q_mrj[0].T[j] @ X_dry[j]
-                            # + q_mrj[1].T[j] @ X_irr[j] ) - d_j[j] ) <= V[j]
-                          # for j in range(nlus) )
-
         # Only add the following constraints if requested.
-
-        # Magic.
-        model.optimize()
 
         # Water use capped, per catchment, at volume consumed in base year.
         if constraints['water']:
@@ -137,6 +137,13 @@ def solve( t_mrj  # Transition cost matrices.
 
         if constraints['biodiversity']:
             ...
+
+        # -------------------------- #
+        # Solve and extract results. #
+        # -------------------------- #
+
+        # Magic.
+        model.optimize()
 
         # Collect optimised decision variables in tuple of 1D Numpy arrays.
         prestack_dry = tuple(X_dry[j].X for j in range(nlus))
@@ -153,7 +160,6 @@ def solve( t_mrj  # Transition cost matrices.
         lmmap = np.where( stack_dry.max(axis=0) >= stack_irr.max(axis=0)
                         , 0, 1 )
 
-
         return lumap, lmmap
 
     except gp.GurobiError as e:
@@ -161,6 +167,16 @@ def solve( t_mrj  # Transition cost matrices.
 
     except AttributeError:
         print('Encountered an attribute error')
+
+        # Non landuse-product-commodity version. For reference:
+        # model.addConstrs( p_j[j]
+                        # * ( d_j[j] - ( q_mrj[0].T[j] @ X_dry[j]
+                                     # + q_mrj[1].T[j] @ X_irr[j] ) ) <= V[j]
+                          # for j in range(nlus) )
+        # model.addConstrs( p_j[j]
+                        # * ( ( q_mrj[0].T[j] @ X_dry[j]
+                            # + q_mrj[1].T[j] @ X_irr[j] ) - d_j[j] ) <= V[j]
+                          # for j in range(nlus) )
 
 if __name__ == '__main__':
 
@@ -170,27 +186,44 @@ if __name__ == '__main__':
     nprs = 7
     ncms = 6
 
-    t_mrj = 10 * np.random.random((nlms, ncells, nlus))
-    q_mrj = 10 * np.random.random((nlms, ncells, nlus))
-    c_mrj = 10 * np.random.random((nlms, ncells, nlus))
-    x_mrj = 10 * np.random.random((nlms, ncells, nlus))
+    t_mrj = np.zeros((nlms, ncells, nlus))
+    t_mrj = 5 * np.ones((nlms, ncells, nlus))
+    t_mrj[:, :, -1] = 0 # LU == 4 everywhere.
 
-    p = 1
+    q_mrp = 1 * np.ones((nlms, ncells, nprs))
+    c_mrj = 1 * np.ones((nlms, ncells, nlus))
+    x_mrj = 1 * np.ones((nlms, ncells, nlus))
 
-    lu2pr_pj = np.array([ [1., 0., 0., 0., 0.]
-                        , [1., 0., 0., 0., 0.]
-                        , [0., 1., 0., 0., 0.]
-                        , [0., 1., 0., 0., 0.]
-                        , [0., 0., 1., 0., 0.]
-                        , [0., 0., 0., 1., 0.]
-                        , [0., 0., 0., 0., 1.]
+    p = 1000
+
+    d_c = np.zeros(ncms)
+    d_c[3] = 1
+
+    lu2pr_pj = np.array([ [1, 0, 0, 0, 0]
+                        , [1, 0, 0, 0, 0]
+                        , [0, 1, 0, 0, 0]
+                        , [0, 1, 0, 0, 0]
+                        , [0, 0, 1, 0, 0]
+                        , [0, 0, 0, 1, 0]
+                        , [0, 0, 0, 0, 1]
                         ])
 
-    pr2cm_cp = np.array([ [1., 1., 0., 0., 0., 0., 0.]
-                        , [0., 0., 1., 0., 0., 0., 0.]
-                        , [0., 0., 0., 1., 0., 0., 0.]
-                        , [0., 0., 0., 0., 1., 0., 0.]
-                        , [0., 0., 0., 0., 0., 1., 0.]
-                        , [0., 0., 0., 0., 0., 0., 1.]
+    pr2cm_cp = np.array([ [1, 0, 0, 0, 0, 0, 0]
+                        , [0, 1, 1, 0, 0, 0, 0]
+                        , [0, 0, 0, 1, 0, 0, 0]
+                        , [0, 0, 0, 0, 1, 0, 0]
+                        , [0, 0, 0, 0, 0, 1, 0]
+                        , [0, 0, 0, 0, 0, 0, 1]
                         ])
 
+    lumap, lmmap = solve( t_mrj
+                        , c_mrj
+                        , q_mrp
+                        , d_c
+                        , p
+                        , x_mrj
+                        , lu2pr_pj
+                        , pr2cm_cp
+                        , constraints = constraints
+                        )
+    print(lumap)
