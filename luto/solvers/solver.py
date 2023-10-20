5bef2c5bc6f9d86d4e2165a69a3cb71107ce292f
nickwilliams <nick.williams@biarri.com>
2023-10-20 16:14:43 +1000

    chore: move clustered solver to separate file

--- luto/solvers/solver.py
+++ luto/solvers/solver.py
@@ -22,13 +22,16 @@ from collections import defaultdict
 import time
 from typing import Optional
 import numpy as np
+import pandas as pd
+import scipy.sparse as sp
 import gurobipy as gp
 from gurobipy import GRB
+from dataclasses import dataclass
+from functools import cached_property
 
 import luto.settings as settings
 from luto import tools
 from luto.ag_managements import AG_MANAGEMENTS_TO_LAND_USES
-from luto.solvers.input_data import InputData
 
 # Set Gurobi environment.
 gurenv = gp.Env(logfilename="gurobi.log", empty=True)  # (empty = True)
@@ -38,6 +41,115 @@ gurenv.setParam("OptimalityTol", settings.OPTIMALITY_TOLERANCE)
 gurenv.setParam("Threads", settings.THREADS)
 gurenv.setParam("BarHomogeneous", settings.BARHOMOGENOUS)
 gurenv.start()
+
+
+@dataclass
+class InputData:
+    """
+    An object that collects and stores all relevant data for solver.py.
+    """
+
+    ag_t_mrj: np.ndarray  # Agricultural transition cost matrices.
+    ag_c_mrj: np.ndarray  # Agricultural production cost matrices.
+    ag_r_mrj: np.ndarray  # Agricultural production revenue matrices.
+    ag_g_mrj: np.ndarray  # Agricultural greenhouse gas emissions matrices.
+    ag_w_mrj: np.ndarray  # Agricultural water requirements matrices.
+    ag_x_mrj: np.ndarray  # Agricultural exclude matrices.
+    ag_q_mrp: np.ndarray  # Agricultural yield matrices -- note the `p` (product) index instead of `j` (land-use).
+    ag_ghg_t_mrj: np.ndarray  # GHG emissions released during transitions between agricultural land uses.
+
+    ag_t_mrj: np.ndarray  # Agricultural transition cost matrices.
+    ag_c_mrj: np.ndarray  # Agricultural production cost matrices.
+    ag_r_mrj: np.ndarray  # Agricultural production revenue matrices.
+    ag_g_mrj: np.ndarray  # Agricultural greenhouse gas emissions matrices.
+    ag_w_mrj: np.ndarray  # Agricultural water requirements matrices.
+    ag_x_mrj: np.ndarray  # Agricultural exclude matrices.
+    ag_q_mrp: np.ndarray  # Agricultural yield matrices -- note the `p` (product) index instead of `j` (land-use).
+    ag_ghg_t_mrj: np.ndarray  # GHG emissions released during transitions between agricultural land uses.
+
+    ag_to_non_ag_t_rk: np.ndarray  # Agricultural to non-agricultural transition cost matrix.
+    non_ag_to_ag_t_mrj: np.ndarray  # Non-agricultural to agricultural transition cost matrices.
+    non_ag_c_rk: np.ndarray  # Non-agricultural production cost matrix.
+    non_ag_r_rk: np.ndarray  # Non-agricultural revenue matrix.
+    non_ag_g_rk: np.ndarray  # Non-agricultural greenhouse gas emissions matrix.
+    non_ag_w_rk: np.ndarray  # Non-agricultural water requirements matrix.
+    non_ag_x_rk: np.ndarray  # Non-agricultural exclude matrices.
+    non_ag_q_crk: np.ndarray  # Non-agricultural yield matrix.
+
+    ag_man_c_mrj: np.ndarray  # Agricultural management options' cost effects.
+    ag_man_g_mrj: np.ndarray  # Agricultural management options' GHG emission effects.
+    ag_man_q_mrp: np.ndarray  # Agricultural management options' quantity effects.
+    ag_man_r_mrj: np.ndarray  # Agricultural management options' revenue effects.
+    ag_man_t_mrj: np.ndarray  # Agricultural management options' transition cost effects.
+    ag_man_w_mrj: np.ndarray  # Agricultural management options' water requirement effects.
+    ag_man_limits: np.ndarray  # Agricultural management options' adoption limits.
+
+    lu2pr_pj: np.ndarray  # Conversion matrix: land-use to product(s).
+    pr2cm_cp: np.ndarray  # Conversion matrix: product(s) to commodity.
+    limits: dict  # Targets to use.
+    desc2aglu: dict  # Map of agricultural land use descriptions to codes.
+
+    @property
+    def n_ag_lms(self):
+        # Number of agricultural landmans
+        return self.ag_t_mrj.shape[0]
+
+    @property
+    def ncells(self):
+        # Number of cells
+        return self.ag_t_mrj.shape[1]
+
+    @property
+    def n_ag_lus(self):
+        # Number of agricultural landuses
+        return self.ag_t_mrj.shape[2]
+
+    @property
+    def n_non_ag_lus(self):
+        # Number of non-agricultural landuses
+        return self.non_ag_c_rk.shape[1]
+
+    @property
+    def nprs(self):
+        # Number of products
+        return self.ag_q_mrp.shape[2]
+
+    @cached_property
+    def am2j(self):
+        # Map of agricultural management options to land use codes
+        return {
+            am: [self.desc2aglu[lu] for lu in am_lus]
+            for am, am_lus in AG_MANAGEMENTS_TO_LAND_USES.items()
+        }
+
+    @cached_property
+    def j2am(self):
+        _j2am = defaultdict(list)
+        for am, am_j_list in self.am2j.items():
+            for j in am_j_list:
+                _j2am[j].append(am)
+
+        return _j2am
+
+    @cached_property
+    def j2p(self):
+        return {
+            j: [p for p in range(self.nprs) if self.lu2pr_pj[p, j]]
+            for j in range(self.n_ag_lus)
+        }
+
+    @cached_property
+    def ag_lu2cells(self):
+        # Make an index of each cell permitted to transform to each land use / land management combination
+        return {
+            (m, j): np.where(self.ag_x_mrj[m, :, j])[0]
+            for j in range(self.n_ag_lus)
+            for m in range(self.n_ag_lms)
+        }
+
+    @cached_property
+    def non_ag_lu2cells(self):
+        return {k: np.where(self.non_ag_x_rk[:, k])[0] for k in range(self.n_non_ag_lus)}
 
 
 class LutoSolver:
@@ -279,7 +391,7 @@ class LutoSolver:
         If `cells` is provided, only adds constraints for the given cells
         """
         if cells is None:
-            cells = np.array(list(self._input_data.cell_clusters.keys()), dtype=int)
+            cells = np.array(range(self._input_data.ncells))
 
         x_ag_dry_vars = self.X_ag_dry_vars_jr[:, cells]
         x_ag_irr_vars = self.X_ag_irr_vars_jr[:, cells]
@@ -648,7 +760,7 @@ class LutoSolver:
         ag_lus_zeros = np.zeros(self._input_data.n_ag_lus)
         non_ag_lus_zeros = np.zeros(self._input_data.n_non_ag_lus)
 
-        for r in self._input_data.cell_clusters.keys():
+        for r in range(self._input_data.ncells):
             old_j = old_lumap[r]
             new_j = current_lumap[r]
             old_m = old_lmmap[r]
@@ -782,8 +894,7 @@ class LutoSolver:
         print("Found optimal objective value", round(self.gurobi_model.objVal, 2), "in", round(ft - st), "seconds\n")
         print("Collecting results...", end=" ", flush=True)
 
-        # Dictionary that stores information about production and GHG emissions for the write module
-        prod_data = {}
+        prod_data = {}  # Dictionary that stores information about production and GHG emissions for the write module
 
         # Collect optimised decision variables in one X_mrj Numpy array.
         X_dry_sol_rj = np.zeros(
@@ -806,34 +917,23 @@ class LutoSolver:
 
         # Get agricultural results
         for j in range(self._input_data.n_ag_lus):
-            for cluster in self._input_data.ag_lu2cells[0, j]:
-                for r in self._input_data.cell_clusters[cluster]:
-                    X_dry_sol_rj[r, j] = self.X_ag_dry_vars_jr[j, cluster].X
-
-            for cluster in self._input_data.ag_lu2cells[1, j]:
-                for r in self._input_data.cell_clusters[cluster]:
-                    X_irr_sol_rj[r, j] = self.X_ag_irr_vars_jr[j, cluster].X
+            for r in self._input_data.ag_lu2cells[0, j]:
+                X_dry_sol_rj[r, j] = self.X_ag_dry_vars_jr[j, r].X
+            for r in self._input_data.ag_lu2cells[1, j]:
+                X_irr_sol_rj[r, j] = self.X_ag_irr_vars_jr[j, r].X
 
         # Get non-agricultural results
         for k in range(self._input_data.n_non_ag_lus):
-            for cluster in self._input_data.non_ag_lu2cells[k]:
-                for r in self._input_data.cell_clusters[cluster]:
-                    non_ag_X_sol_rk[r, k] = self.X_non_ag_vars_kr[k, cluster].X
+            for r in self._input_data.non_ag_lu2cells[k]:
+                non_ag_X_sol_rk[r, k] = self.X_non_ag_vars_kr[k, r].X
 
         # Get agricultural management results
         for am, am_j_list in self._input_data.am2j.items():
             for j_idx, j in enumerate(am_j_list):
-                for cluster in self._input_data.ag_lu2cells[0, j]:
-                    for r in self._input_data.cell_clusters[cluster]:
-                        am_X_dry_sol_rj[am][r, j] = self.X_ag_man_dry_vars_jr[am][
-                            j_idx, cluster
-                        ].X
-
-                for cluster in self._input_data.ag_lu2cells[1, j]:
-                    for r in self._input_data.cell_clusters[cluster]:
-                        am_X_irr_sol_rj[am][r, j] = self.X_ag_man_irr_vars_jr[am][
-                            j_idx, cluster
-                        ].X
+                for r in self._input_data.ag_lu2cells[0, j]:
+                    am_X_dry_sol_rj[am][r, j] = self.X_ag_man_dry_vars_jr[am][j_idx, r].X
+                for r in self._input_data.ag_lu2cells[1, j]:
+                    am_X_irr_sol_rj[am][r, j] = self.X_ag_man_irr_vars_jr[am][j_idx, r].X
 
         """Note that output decision variables are mostly 0 or 1 but in some cases they are somewhere in between which creates issues 
             when converting to maps etc. as individual cells can have non-zero values for multiple land-uses and land management type.
@@ -865,17 +965,17 @@ class LutoSolver:
 
         # Process non-agricultural land usage information
         # Boolean matrix where the maximum value for each cell across all non-ag LUs is True
-        # non_ag_X_rk_processed = non_ag_X_sol_rk.argmax(axis=1)[:, np.newaxis] == range(
-        #     self._input_data.n_non_ag_lus
-        # )
+        non_ag_X_rk_processed = non_ag_X_sol_rk.argmax(axis=1)[:, np.newaxis] == range(
+            self._input_data.n_non_ag_lus
+        )
 
         # Make land use and land management maps
         # Vector indexed by cell that denotes whether the cell is non-agricultural land (True) or agricultural land (False)
         non_ag_bools_r = non_ag_X_sol_rk.max(axis=1) > ag_X_mrj.max(axis=(0, 2))
 
         # Update processed variables accordingly
-        # ag_X_mrj_processed[:, non_ag_bools_r, :] = False
-        # non_ag_X_rk_processed[~non_ag_bools_r, :] = False
+        ag_X_mrj_processed[:, non_ag_bools_r, :] = False
+        non_ag_X_rk_processed[~non_ag_bools_r, :] = False
 
         # Process agricultural management variables
         # Repeat the steps for the regular agricultural management variables
@@ -935,41 +1035,14 @@ class LutoSolver:
 
         # # Process production amount for each commodity
         print("Processing commodity production amounts...")
-        prod_data["Production"] = [0 for _ in range(self.ncms)]
-        ag_X_mrp = self.get_ag_X_mrp(ag_X_mrj_processed)
-        product_amounts = [0 for _ in range(self._input_data.nprs)]
-        
-        # Add agricultural contributions
-        for p in range(self._input_data.nprs):
-            
-            ag_contr = (ag_X_mrp[:, :, p] * self._input_data.ag_q_mrp[:, :, p]).sum()
-            product_amounts[p] += ag_contr
-        
-        # Add agricultural management contributions
-        for am in AG_MANAGEMENTS_TO_LAND_USES:
-            ag_man_X_mrp = self.get_ag_X_mrp(ag_man_X_mrj_processed[am])
-            for p in range(self._input_data.nprs):
-                ag_man_contr = (ag_man_X_mrp[:, :, p] * self._input_data.ag_man_q_mrp[am][:, :, p]).sum()
-                product_amounts[p] += ag_man_contr
-
-        # Add non-agricultural contributions and convert to commodities
-        for c in range(self.ncms):
-            # Convert product amounts to commodity amounts
-            commodity_amount = 0
-            for p in range(self._input_data.nprs):
-                if self._input_data.pr2cm_cp[c, p]:
-                    commodity_amount += product_amounts[p]
-
-            # Add non-agricultural contribution
-            for k in range(self._input_data.n_non_ag_lus):
-                non_ag_contr = (non_ag_X_sol_rk[:, k] * self._input_data.non_ag_q_crk[c, :, k]).sum()
-                commodity_amount += non_ag_contr
-
-            prod_data["Production"][c] = commodity_amount
-
+        prod_data["Production"] = [self.total_q_exprs_c[c].getValue() for c in range(self.ncms)]
 
         print("Processing GHG emissions amount...")
         prod_data["GHG Emissions"] = self.ghg_emissions_expr.getValue()
+
+        ag_X_mrj_processed[:, non_ag_bools_r, :] = False
+        non_ag_X_rk_processed[~non_ag_bools_r, :] = False
+
         print("Done\n")
 
         return (
@@ -1009,17 +1082,3 @@ class LutoSolver:
             for j in range(self._input_data.n_ag_lus)
             if self._input_data.lu2pr_pj[p, j]
         ]
-    
-    def get_ag_X_mrp(self, ag_X_mrj: np.ndarray) -> np.ndarray:
-        """
-        Transform the agricultural solution matrix to mrp form for production processing
-        """
-        ag_X_mrp = np.zeros(
-            (self._input_data.n_ag_lms, self._input_data.ncells, self._input_data.nprs)
-        )
-        for m in range(self._input_data.n_ag_lms):
-            for j in range(self._input_data.n_ag_lus):
-                for p in self._input_data.j2p[j]:
-                    ag_X_mrp[m, :, p] = ag_X_mrj[m, :, j]
-
-        return ag_X_mrp
