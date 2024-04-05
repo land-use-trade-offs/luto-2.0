f5976ee01a9f0a2af2ae17c55988f66eb1f86e16
jinzhu <wangjinzhulala@gmail.com>
2024-04-05 12:27:47 +1100

    Add the new non-ag to reports

--- luto/tools/report/data_tools/parameters.py
+++ luto/tools/report/data_tools/parameters.py
@@ -1,3 +1,4 @@
+
 # Get the root directory of the data
 YR_BASE = 2010
 
@@ -56,12 +57,16 @@ AG_LANDUSE = ['Apples', 'Beef - modified land', 'Beef - natural land', 'Citrus',
               'Summer legumes', 'Summer oilseeds', 'Tropical stone fruit', 'Unallocated - modified land', 
               'Unallocated - natural land', 'Vegetables', 'Winter cereals', 'Winter legumes', 'Winter oilseeds']
 
+
+# Read the non-agricultural land uses from the file
+with open('input/non_ag_landuses.csv') as f:
+    NON_AG_LANDUSE = [line.strip() for line in f]
+    
+    
 AG_LANDUSE_MERGE_LANDTYPE = ['Apples', 'Beef', 'Citrus', 'Cotton', 'Dairy', 'Grapes', 'Hay', 'Nuts', 'Other non-cereal crops',
                              'Pears', 'Plantation fruit', 'Rice', 'Sheep', 'Stone fruit', 'Sugar', 'Summer cereals',
                              'Summer legumes', 'Summer oilseeds', 'Tropical stone fruit', 'Unallocated - modified land', 
                              'Unallocated - natural land', 'Vegetables', 'Winter cereals', 'Winter legumes', 'Winter oilseeds']
-
-NON_AG_LANDUSE = ['Environmental Plantings','Riparian Plantings','Agroforestry']
 
 LANDUSE_ALL = AG_LANDUSE + NON_AG_LANDUSE
 
@@ -95,6 +100,8 @@ GHG_NAMES = {
     'TCO2E_Agroforestry': 'Agroforestry', 
     'TCO2E_Environmental Plantings': 'Environmental Plantings',
     'TCO2E_Riparian Plantings': 'Riparian Plantings',
+    'TCO2E_Carbon Plantings (Belt)': 'Carbon Plantings (Belt)',
+    'TCO2E_Carbon Plantings (Block)': 'Carbon Plantings (Block)',
 }
 
 GHG_CATEGORY = {'Agricultural soils: Animal production, dung and urine': {"CH4":0.5,"CO2":0.5},
