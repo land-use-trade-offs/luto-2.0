49363c7b8f3aa49eafb60d2990dcf4f57f0a794b
Brett A Bryan <brett.a.bryan@gmail.com>
2024-04-19 08:36:29 +1000

    Update settings and helper

--- luto/settings.py
+++ luto/settings.py
@@ -128,7 +128,11 @@ WRITE_OUTPUT_GEOTIFFS = False
 PARALLEL_WRITE = True
 
 # The Threads to use for writing GeoTiffs, and map making
+<<<<<<< Updated upstream
 WRITE_THREADS = 100      # Works only if PARALLEL_WRITE = True
+=======
+WRITE_THREADS = 50      # Works only if PARALLEL_WRITE = True
+>>>>>>> Stashed changes
 
 
 # ---------------------------------------------------------------------------- #
