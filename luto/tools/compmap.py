dce3bf7d2bae7a047dc67f09b5856f9f84005c58
jinzhu <wangjinzhulala@gmail.com>
2024-04-19 05:51:59 +1000

    Correcting the txt split i crosstab

--- luto/tools/compmap.py
+++ luto/tools/compmap.py
@@ -147,9 +147,9 @@ def crossmap_irrstat( lumap_old
 
     # Rearrange crosstab to long format
     crosstab = crosstab.stack().reset_index().rename(columns={0: 'Area (km2)'})
-    crosstab[['From land-use','From water_supply', 'tmp']] = crosstab['level_0'].str.split(' \(', expand=True)
-    crosstab[['To land-use', 'To water_supply', 'tmp']] = crosstab['level_1'].str.split(' \(', expand=True)
-    crosstab = crosstab.replace('irr)', 'Irrigated').replace('dry)', 'Dryland').drop(['level_0','level_1', 'tmp'], axis=1)
+    crosstab[['From land-use','From water_supply','tmp' ]] = crosstab['level_0'].str.split('\((dry|irr)', expand=True)
+    crosstab[['To land-use', 'To water_supply', 'tmp']] = crosstab['level_1'].str.split('\((dry|irr)', expand=True)
+    crosstab = crosstab.replace('irr', 'Irrigated').replace('dry', 'Dryland').drop(['level_0','level_1', 'tmp' ], axis=1)
 
     return crosstab, df
 
