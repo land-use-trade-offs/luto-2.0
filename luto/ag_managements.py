a82d782f19bbf9b989bae3bb77f41193c4aced7c
Nick Williams <nick.williams@biarri.com>
2024-04-18 14:04:14 +1000

    feat: add initial support for disabling ag man options

--- luto/ag_managements.py
+++ luto/ag_managements.py
@@ -1,3 +1,17 @@
+"""
+Contains a master list of all agricultural management options and
+which land uses they correspond to. 
+
+To disable an ag-mangement option, change the corresponding value in the
+AG_MANAGEMENTS dictionary to False.
+"""
+
+AG_MANAGEMENTS = {
+    'Asparagopsis taxiformis': True,
+    'Precision Agriculture': True,
+    'Ecological Grazing': True,
+}
+
 AG_MANAGEMENTS_TO_LAND_USES = {
     'Asparagopsis taxiformis': [
         'Beef - natural land',
--- luto/non_ag_landuses.py
+++ luto/non_ag_landuses.py
@@ -1,9 +1,16 @@
-# This dict stores all of the non agricultural land uses and whether they are currently enabled in the solver.
+"""
+Contains a master dictionary that stores all of the non agricultural land uses
+and whether they are currently enabled in the solver (True/False). 
+
+To disable a non-agricultural land use, change the correpsonding value of the
+NON_AG_LAND_USES dictionary to false.
+"""
+
 NON_AG_LAND_USES = {
     'Environmental Plantings': True,
     'Riparian Plantings': True,
     'Agroforestry': True,
-    'Carbon Plantings (Block)': False,
+    'Carbon Plantings (Block)': True,
     'Carbon Plantings (Belt)': True,
     'BECCS': True,
 }
