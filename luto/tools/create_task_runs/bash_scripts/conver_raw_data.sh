358e0162e6d2ab144036783ffa862731354dda4d
jinzhu <wangjinzhulala@gmail.com>
2024-05-01 13:00:30 +1000

    Add conda actiavte in raw data conversion script

--- luto/tools/create_task_runs/bash_scripts/conver_raw_data.sh
+++ luto/tools/create_task_runs/bash_scripts/conver_raw_data.sh
@@ -1,5 +1,8 @@
 # Run the simulation
 python <<-INNER_EOF
+# Activate the Conda environment
+source ~/miniforge3/etc/profile.d/conda.sh
+conda activate luto
 from luto.dataprep import create_new_dataset
 create_new_dataset()
 INNER_EOF
