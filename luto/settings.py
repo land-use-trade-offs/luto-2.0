9b7435d9ece920ddd420ad23b057df7123c6a7df
Nick Williams <nick.williams@biarri.com>
2024-07-04 15:19:58 +1000

    fix: settings revert

--- luto/settings.py
+++ luto/settings.py
@@ -119,10 +119,10 @@ PENALTY = 1e5
 # Geographical raster writing parameters
 # ---------------------------------------------------------------------------- #
 
-WRITE_OUTPUT_GEOTIFFS = False    # Write GeoTiffs to output directory: True or False
+WRITE_OUTPUT_GEOTIFFS = True    # Write GeoTiffs to output directory: True or False
 WRITE_FULL_RES_MAPS = False     # Write GeoTiffs at full or resfactored resolution: True or False
-PARALLEL_WRITE = False           # If to use parallel processing to write GeoTiffs: True or False
-WRITE_THREADS = 8              # The Threads to use for map making, only work with PARALLEL_WRITE = True
+PARALLEL_WRITE = True           # If to use parallel processing to write GeoTiffs: True or False
+WRITE_THREADS = 50              # The Threads to use for map making, only work with PARALLEL_WRITE = True
 
 # ---------------------------------------------------------------------------- #
 # Gurobi parameters
@@ -152,7 +152,7 @@ NUMERIC_FOCUS = 0   # Controls the degree to which the code attempts to detect a
 BARHOMOGENOUS = -1  # Useful for recognizing infeasibility or unboundedness. At the default setting (-1), it is only used when barrier solves a node relaxation for a MIP model. 0 = off, 1 = on. It is a bit slower than the default algorithm (3x slower in testing).
 
 # Number of threads to use in parallel algorithms (e.g., barrier)
-THREADS = 8
+THREADS = 50
 
 
 # ---------------------------------------------------------------------------- #
@@ -332,6 +332,7 @@ WATER_NET_YIELD_LIMITS = 'on'               # 'on' or 'off'
 
 WATER_LIMITS_TARGET_YEAR = 2030
 
+# Safe and just Earth system boundaries says 0.2 inclusive of domestic/industrial https://www.nature.com/articles/s41586-023-06083-8
 WATER_STRESS_FRACTION = 0.2          
 
 # Regionalisation to enforce water use limits by
