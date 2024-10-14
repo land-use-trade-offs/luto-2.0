96f6759c7822976256a42f3a57015d535e9e118a
Jinzhu <wangjinzhulala@gmail.com>
2024-10-14 15:58:56 +1100

    update the script for creating `luto` python environment

--- luto/tools/create_task_runs/bash_scripts/install_pkg.sh
+++ luto/tools/create_task_runs/bash_scripts/install_pkg.sh
@@ -3,16 +3,6 @@
 # Add the Conda binaries to the PATH
 source ~/.bashrc
 
-py_ver='python=3.12'
-py_ver_=$(echo $py_ver | sed 's/=//g')
-
 # Create a new Conda environment, and install the required packages
-mamba create -c conda-forge -n luto ${py_ver} -y
-mamba activate luto
+mamba env create -f conda_env.yml
 
-
-# Install packages using conda in the "luto" environment
-mamba install -n luto -c conda-forge -y $(cat requirements_conda.txt)
-
-# Install packages using pip in the "luto" environment
-pip install --target=$HOME/miniforge3/envs/luto/lib/${py_ver_}/site-packages $(cat requirements_pip.txt)
