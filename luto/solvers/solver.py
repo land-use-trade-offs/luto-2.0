9be5dac643af23038792aa1ddd4151d1f653f490
Jinzhu <wangjinzhulala@gmail.com>
2025-02-10 15:40:41 +1100

    minor fix on objective function when MODE is 'mincost'

--- luto/solvers/solver.py
+++ luto/solvers/solver.py
@@ -281,21 +281,21 @@ class LutoSolver:
         
         # Get the objective values for each sector
         self.obj_economy = ag_obj_contr + ag_man_obj_contr + non_ag_obj_contr
+        self.obj_ghg = self.E * self._input_data.economic_target_yr_carbon_price                                        if settings.GHG_CONSTRAINT_TYPE == "soft" else 0
+        self.obj_water = self.W.sum() * settings.WATER_PENALTY                                                          if settings.WATER_CONSTRAINT_TYPE == "soft" else 0
+        self.obj_demand = gp.quicksum(v * price for v, price in zip(self.V, self._input_data.economic_BASE_YR_prices))  if settings.DEMAND_CONSTRAINT_TYPE == "soft" else 0
 
-        if settings.DEMAND_CONSTRAINT_TYPE == "soft":
-            self.obj_demand = gp.quicksum(v * price for v, price in zip(self.V, self._input_data.economic_BASE_YR_prices))
-        else:
-            self.obj_demand = 0
-        
-        self.obj_ghg = self.E * self._input_data.economic_target_yr_carbon_price    if settings.GHG_CONSTRAINT_TYPE == "soft" else 0
-        self.obj_water = self.W.sum() * settings.WATER_PENALTY                      if settings.WATER_CONSTRAINT_TYPE == "soft" else 0
 
         # Set the objective function
-        sense = GRB.MINIMIZE if settings.OBJECTIVE == "mincost" else GRB.MAXIMIZE
+        if settings.OBJECTIVE == "mincost":
+            sense = GRB.MINIMIZE
+            objective = self.obj_economy *  settings.SOLVE_ECONOMY_WEIGHT + (self.obj_demand +  self.obj_ghg + self.obj_water) * (1 - settings.SOLVE_ECONOMY_WEIGHT)
+        elif settings.OBJECTIVE == "maxprofit":
+            sense = GRB.MAXIMIZE
+            objective = self.obj_economy *  settings.SOLVE_ECONOMY_WEIGHT - (self.obj_demand +  self.obj_ghg + self.obj_water) * (1 - settings.SOLVE_ECONOMY_WEIGHT)
+        else:
+            raise ValueError(f"Unknown objective function: {settings.OBJECTIVE}")
         
-        objective = self.obj_economy *  settings.SOLVE_ECONOMY_WEIGHT \
-            - (self.obj_demand +  self.obj_ghg + self.obj_water) * (1 - settings.SOLVE_ECONOMY_WEIGHT)
-                 
         self.gurobi_model.setObjective(objective, sense)
         
         
