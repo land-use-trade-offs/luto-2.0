fix ag/non transition cost calculation

luto/economics/agricultural/transitions.py
@@ -142,7 +142,7 @@ def get_transition_matrices(data: Data, yr_idx, base_year, separate=False):
     e_mrj = np.stack([e_rj, e_rj], axis=0)
 
     # Update the cost matrix with exclude matrices; the transition cost for a cell that remain the same is 0.
-    e_mrj = np.einsum('mrj,mrj,mrj->mrj', e_mrj, x_mrj, l_mrj_not)
+    e_mrj = np.einsum('mrj,mrj,mrj->mrj', e_mrj, x_mrj, l_mrj_not).astype(np.float32)
 
     # -------------------------------------------------------------- #
     # Water license cost (upfront, amortised to annual, per cell).   #
@@ -150,7 +150,7 @@ def get_transition_matrices(data: Data, yr_idx, base_year, separate=False):
 
     w_mrj = get_wreq_matrices(data, yr_idx)                                     # <unit: ML/cell>
     w_delta_mrj = tools.get_water_delta_matrix(w_mrj, l_mrj, data, yr_idx)
-    w_delta_mrj = np.einsum('mrj,mrj,mrj->mrj', w_delta_mrj, x_mrj, l_mrj_not)
+    w_delta_mrj = np.einsum('mrj,mrj,mrj->mrj', w_delta_mrj, x_mrj, l_mrj_not).astype(np.float32)
 
     # -------------------------------------------------------------- #
     # Carbon costs of transitioning cells.                           #
@@ -158,10 +158,20 @@ def get_transition_matrices(data: Data, yr_idx, base_year, separate=False):
 
     # Apply the cost of carbon released by transitioning natural land to modified land
     ghg_transition = ag_ghg.get_ghg_transition_penalties(data, lumap, separate=True)        # <unit: t/ha>
+    
+    ghg_transition = {
+        k:np.einsum('mrj,mrj,mrj->mrj', v, x_mrj, l_mrj_not).astype(np.float32)             # No GHG penalty for cells that remain the same, or are prohibited from transitioning
+        for k, v in ghg_transition.items()
+    }
+    
+    ghg_transition = {
+    k:tools.amortise(v * data.get_carbon_price_by_yr_idx(yr_idx))                       # Amortise the GHG penalties
+    for k,v in ghg_transition.items()
+    }
+    
     ghg_t_types = ghg_transition.keys()
     ghg_t_smrj = np.stack([ghg_transition[t] for t in ghg_t_types], axis=0)                 # s: ghg_t_types, m: land management, r: cell, j: land use
-    ghg_t_smrj = tools.amortise(ghg_t_smrj * data.get_carbon_price_by_yr_idx(yr_idx))
-    ghg_t_smrj_cost = np.einsum('smrj,mrj,mrj->smrj', ghg_t_smrj, x_mrj, l_mrj_not)
+    ghg_t_mrj = np.einsum('smrj->mrj', ghg_t_smrj)
 
     # -------------------------------------------------------------- #
     # Total costs.                                                   #
@@ -170,7 +180,7 @@ def get_transition_matrices(data: Data, yr_idx, base_year, separate=False):
     if separate:
         return {'Establishment cost': e_mrj, 'Water license cost': w_delta_mrj, **ghg_transition}
     else:
-        return e_mrj + w_delta_mrj + np.einsum('smrj->mrj', ghg_t_smrj_cost)
+        return e_mrj + w_delta_mrj + ghg_t_mrj
 
 
 def get_asparagopsis_effect_t_mrj(data: Data):

luto/economics/non_agricultural/transitions.py
@@ -66,9 +66,9 @@ def get_env_plant_transitions_from_ag(data: Data, yr_idx, lumap, lmmap, separate
 
 
     if separate:
-        return {'Transition cost': np.einsum('mrj,mrj,r->mrj', base_ag_to_ep_t_mrj, l_mrj, data.REAL_AREA), 
-                'Establishment cost': np.einsum('r,mrj,r->mrj', est_costs_r, l_mrj, data.REAL_AREA),
-                'Water license cost': np.einsum('mrj,mrj,r->mrj', w_delta_mrj, l_mrj, data.REAL_AREA)}
+        return {'Ag2Non-Ag Transition cost': np.einsum('mrj,mrj,r->mrj', base_ag_to_ep_t_mrj, l_mrj, data.REAL_AREA), 
+                'Ag2Non-Ag Establishment cost': np.einsum('r,mrj,r->mrj', est_costs_r, l_mrj, data.REAL_AREA),
+                'Ag2Non-Ag Water license cost': np.einsum('mrj,mrj,r->mrj', w_delta_mrj, l_mrj, data.REAL_AREA)}
         
     ag2ep_transitions_r += est_costs_r
     return ag2ep_transitions_r * data.REAL_AREA
@@ -93,7 +93,7 @@ def get_rip_plant_transitions_from_ag(data: Data, yr_idx, lumap, lmmap, separate
     
     if separate:
         l_mrj = lumap2ag_l_mrj(lumap, lmmap)
-        base_costs.update({'Fencing cost':np.einsum('r,mrj->mrj', fencing_cost, l_mrj)})
+        base_costs.update({'Ag2Non-Ag Fencing cost':np.einsum('r,mrj->mrj', fencing_cost, l_mrj)})
         return base_costs
     else:
         return base_costs + fencing_cost
@@ -118,7 +118,7 @@ def get_agroforestry_transitions_from_ag_base(data: Data, yr_idx, lumap, lmmap, 
     
     if separate:
         l_mrj = lumap2ag_l_mrj(lumap, lmmap)
-        base_costs.update({'Fencing cost':np.einsum('r,mrj->mrj', fencing_cost, l_mrj)})
+        base_costs.update({'Ag2Non-Ag Fencing cost':np.einsum('r,mrj->mrj', fencing_cost, l_mrj)})
         return base_costs
     else:
         return base_costs + fencing_cost
@@ -190,7 +190,7 @@ def get_beef_agroforestry_transitions_from_ag(
     
     if separate:
         # Combine and return separated costs
-        # Agroforestry keys: 'Transition cost', 'Establishment cost', 'Water license cost', 'Fencing cost'
+        # Agroforestry keys: 'Ag2Non-Ag Transition cost', 'Ag2Non-Ag Establishment cost', 'Ag2Non-Ag Water license cost', 'Ag2Non-Ag Fencing cost'
         combined_costs = {}
         for key, array in agroforestry_costs.items():
             combined_costs[key] = np.zeros(array.shape).astype(np.float32)
@@ -216,7 +216,7 @@ def get_beef_agroforestry_transitions_from_ag(
         agroforestry_contr = agroforestry_costs * agroforestry_x_r
         t_r = beef_contr + agroforestry_contr
 
-        # Set all non-agricultural land to have zero
+        # Set all non-agricultural land to have zero cost
         non_ag_cells = tools.get_non_ag_cells(lumap)
         t_r[non_ag_cells] = 0
 
@@ -256,9 +256,9 @@ def get_carbon_plantings_block_from_ag(data: Data, yr_idx, lumap, lmmap, separat
     ag2cp_transitions_r += est_costs_r
 
     if separate:
-        return {'Transition cost':np.einsum('mrj,mrj,r->mrj', base_ag_to_cp_t_mrj, l_mrj, data.REAL_AREA), 
-                'Establishment cost': np.einsum('r,mrj,r->mrj', est_costs_r, l_mrj, data.REAL_AREA),
-                'Water license cost': np.einsum('mrj,mrj,r->mrj', w_delta_mrj, l_mrj, data.REAL_AREA)}
+        return {'Ag2Non-Ag Transition cost':np.einsum('mrj,mrj,r->mrj', base_ag_to_cp_t_mrj, l_mrj, data.REAL_AREA), 
+                'Ag2Non-Ag Establishment cost': np.einsum('r,mrj,r->mrj', est_costs_r, l_mrj, data.REAL_AREA),
+                'Ag2Non-Ag Water license cost': np.einsum('mrj,mrj,r->mrj', w_delta_mrj, l_mrj, data.REAL_AREA)}
     else:
         return ag2cp_transitions_r * data.REAL_AREA
 
@@ -284,7 +284,7 @@ def get_carbon_plantings_belt_from_ag_base(data: Data, yr_idx, lumap, lmmap, sep
     
     if separate:
         l_mrj = lumap2ag_l_mrj(lumap, lmmap)
-        base_costs.update({'Fencing cost':np.einsum('r,mrj->mrj', fencing_cost, l_mrj)})
+        base_costs.update({'Ag2Non-Ag Fencing cost':np.einsum('r,mrj->mrj', fencing_cost, l_mrj)})
         return base_costs
     else:
         return base_costs + fencing_cost
@@ -505,8 +505,8 @@ def get_env_plantings_to_ag(data: Data, yr_idx, lumap, lmmap, separate=False) ->
     base_ep_to_ag_t_mrj[:, ag_cells, :] = 0
 
     if separate:
-        return {'Transition cost':np.einsum('mrj,mrj,r->mrj', base_ep_to_ag_t_mrj, l_mrj, data.REAL_AREA), 
-                'Water license cost': np.einsum('mrj,mrj,r->mrj', w_delta_mrj, l_mrj, data.REAL_AREA)}
+        return {'Non-Ag2Ag Transition cost':np.einsum('mrj,mrj,r->mrj', base_ep_to_ag_t_mrj, l_mrj, data.REAL_AREA), 
+                'Non-Ag2Ag Water license cost': np.einsum('mrj,mrj,r->mrj', w_delta_mrj, l_mrj, data.REAL_AREA)}
         
     # Add cost of water license and cost of installing/removing irrigation where relevant (pre-amortised)
     ep_to_ag_t_mrj = base_ep_to_ag_t_mrj + w_delta_mrj
@@ -600,7 +600,7 @@ def get_sheep_to_ag_base(data: Data, yr_idx: int, lumap, separate=False) -> np.n
     ghg_t_mrj_cost[:, ag_cells, :] = np.zeros((data.NLMS, ag_cells.shape[0], data.N_AG_LUS)).astype(np.float32)
 
     if separate:
-        return {'Establishment cost': e_mrj, 'Water license cost': w_delta_mrj, 'GHG emissions cost': ghg_t_mrj_cost}
+        return {'Non-Ag2Ag Establishment cost': e_mrj, 'Water license cost': w_delta_mrj, 'GHG emissions cost': ghg_t_mrj_cost}
     
     else:
         return e_mrj + w_delta_mrj + ghg_t_mrj_cost
@@ -659,7 +659,7 @@ def get_beef_to_ag_base(data: Data, yr_idx, lumap, separate) -> np.ndarray|dict:
     ghg_t_mrj_cost[:, ag_cells, :] = np.zeros((data.NLMS, ag_cells.shape[0], data.N_AG_LUS)).astype(np.float32)
 
     if separate:
-        return {'Establishment cost': e_mrj, 'Water license cost': w_delta_mrj, 'GHG emissions cost': ghg_t_mrj_cost}
+        return {'Non-Ag2Ag Establishment cost': e_mrj, 'Non-Ag2Ag Water license cost': w_delta_mrj, 'Non-Ag2Ag GHG emissions cost': ghg_t_mrj_cost}
     
     else:
         t_mrj = e_mrj + w_delta_mrj + ghg_t_mrj_cost

luto/tools/write.py
@@ -69,7 +69,7 @@ from luto.tools.report.create_static_maps import TIF2MAP
 
 
 # Global timestamp for the run
-timestamp = tools.get_timestamp()
+timestamp = tools.write_timestamp()
           
 def write_outputs(data: Data):
     """Write model outputs to file"""
@@ -130,7 +130,8 @@ def move_logs(data: Data):
             f"{settings.OUTPUT_DIR}/run_{timestamp}_stderr.log",
             f"{settings.OUTPUT_DIR}/write_{timestamp}_stdout.log",
             f"{settings.OUTPUT_DIR}/write_{timestamp}_stderr.log",
-            f'{settings.OUTPUT_DIR}/RES_{settings.RESFACTOR}_{settings.MODE}_mem_log.txt']
+            f'{settings.OUTPUT_DIR}/RES_{settings.RESFACTOR}_{settings.MODE}_mem_log.txt',
+            f'{settings.OUTPUT_DIR}/.timestamp']
 
     for log in logs:
         try:
@@ -523,21 +524,26 @@ def write_cost_transition(data: Data, yr_cal, path, yr_cal_sim_pre=None):
 
     cost_dfs = []
     # Convert the transition cost matrices to a DataFrame
-    for lu_desc, lu_idx in data.DESC2AGLU.items():
-        for cost_type in ag_transitions_cost_mat.keys():
+    for from_lu in data.AGRICULTURAL_LANDUSES:
+        for from_lm in data.LANDMANS:
+            for cost_type in ag_transitions_cost_mat.keys():
+                            
+                from_lu_idx = base_mrj[data.LANDMANS.index(from_lm), :, data.DESC2AGLU[from_lu]]    # Get the cells of 'from' land-use in the base year (r)
+                arr_trans = np.nan_to_num(ag_transitions_cost_mat[cost_type])[:,from_lu_idx,:]      # Get the transition cost matrix                    (m,r*,j)
+                arr_dvar = ag_dvar[:,from_lu_idx,:]                                                 # Get the decision variables                        (m,r*,j)                          
+                
+                cost_arr = np.einsum('mrj,mrj->mj', arr_trans, arr_dvar)                            # Multiply by decision variables
 
-            base_lu_arr = base_mrj[:, :, lu_idx]                                      # Get the base land-use array                       (m,r)
-            arr = np.nan_to_num(ag_transitions_cost_mat[cost_type])                   # Get the transition cost matrix                    (m,r,j)
-            arr = np.einsum('mr,mrj,mrj->mj', base_lu_arr, arr, ag_dvar)              # Multiply by decision variables
-
-            arr_df = pd.DataFrame(arr.flatten(),
-                            index=pd.MultiIndex.from_product([data.LANDMANS, data.AGRICULTURAL_LANDUSES],
-                            names=['Water Supply', 'To land-use']),
-                            columns=['Cost ($)']).reset_index()
-            arr_df.insert(0, 'Type', cost_type)
-            arr_df.insert(1, 'Year', yr_cal)
-            arr_df.insert(2, 'From land-use', lu_desc)
-            cost_dfs.append(arr_df)
+                arr_df = pd.DataFrame(
+                    cost_arr.flatten(),
+                    index=pd.MultiIndex.from_product([data.LANDMANS, data.AGRICULTURAL_LANDUSES],
+                    names=['To water-upply', 'To land-use']),
+                    columns=['Cost ($)']).reset_index()
+                arr_df.insert(0, 'Type', cost_type)
+                arr_df.insert(1, 'Year', yr_cal)
+                arr_df.insert(2, 'From land-use', from_lu)
+                arr_df.insert(3, 'From water-supply', from_lm)
+                cost_dfs.append(arr_df)
 
     # Save the cost DataFrames
     cost_df = pd.concat(cost_dfs, axis=0)
@@ -564,27 +570,39 @@ def write_cost_transition(data: Data, yr_cal, path, yr_cal_sim_pre=None):
     # Get the transition cost matirces for non-agricultural land-use
     if yr_idx == 0:
         non_ag_transitions_cost_mat = {
-            k:{'Transition cost':np.zeros((data.NLMS, data.NCELLS, data.N_AG_LUS)).astype(np.float32)}
+            k:{'Transition cost':np.zeros((data.NCELLS, data.N_AG_LUS)).astype(np.float32)}
             for k in NON_AG_LAND_USES.keys()
         }
     else:
         non_ag_transitions_cost_mat = non_ag_transitions.get_from_ag_transition_matrix(
             data,yr_idx, yr_cal_sim_pre, data.lumaps[yr_cal_sim_pre], data.lmmaps[yr_cal_sim_pre], separate=True
         )
+        
+
 
     cost_dfs = []
-    for idx,non_ag_type in enumerate(non_ag_transitions_cost_mat):
-        for cost_type in non_ag_transitions_cost_mat[non_ag_type]:
-            arr = non_ag_transitions_cost_mat[non_ag_type][cost_type]          # Get the transition cost matrix
-            arr = np.einsum('mrj,r->mj', arr, non_ag_dvar[:,idx])              # Multiply the transition cost matrix by the cost of non-agricultural land-use
-
-            arr_df = pd.DataFrame(arr.flatten(),
-                                index=pd.MultiIndex.from_product([data.LANDMANS, data.AGRICULTURAL_LANDUSES],names=['Water supply', 'From land-use']),
-                                columns=['Cost ($)']).reset_index()
-            arr_df.insert(0, 'To land-use', non_ag_type)
-            arr_df.insert(1, 'Cost type', cost_type)
-            arr_df.insert(2, 'Year', yr_cal)
-            cost_dfs.append(arr_df)
+    for from_lu in data.AGRICULTURAL_LANDUSES:
+        for from_lm in data.LANDMANS:
+            for to_lu in NON_AG_LAND_USES.keys():
+                for cost_type in non_ag_transitions_cost_mat[to_lu].keys():
+                    
+                    from_lu_idx = base_mrj[data.LANDMANS.index(from_lm), :, data.DESC2AGLU[from_lu]]                                                    # Get the land-use index of the from land-use (r*)
+                    arr_trans = non_ag_transitions_cost_mat[to_lu][cost_type][data.LANDMANS.index(from_lm), from_lu_idx, data.DESC2AGLU[from_lu]]       # Get the transition cost matrix of from land-use (r*)
+                    arr_dvar = non_ag_dvar[from_lu_idx, data.NON_AGRICULTURAL_LANDUSES.index(to_lu)]                                                    # Get the decision variable of the from land-use (r*,k)
+                    
+                    if arr_dvar.size == 0:
+                        continue
+                    
+                    cost_arr = np.einsum('r,r->', arr_trans, arr_dvar)                                # Calculate the cost array
+                    arr_df = pd.DataFrame([{
+                        'Cost type': cost_type,
+                        'From land-use': from_lu,
+                        'From water-supply': from_lm,
+                        'To land-use': to_lu,
+                        'Cost ($)': cost_arr,
+                        'Year': yr_cal
+                    }])
+                    cost_dfs.append(arr_df)
 
     # Save the cost DataFrames
     cost_df = pd.concat(cost_dfs, axis=0)