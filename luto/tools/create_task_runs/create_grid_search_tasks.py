18c7946efd51e168bea05ff453428771177b6356
Jinzhu WANG <wangjinzhulala@gmail.com>
2025-06-10 12:31:33 +1000

    chore: modify the task run plots

--- luto/tools/create_task_runs/create_grid_search_tasks.py
+++ luto/tools/create_task_runs/create_grid_search_tasks.py
@@ -27,7 +27,7 @@ from luto.tools.create_task_runs.helpers import (
 )
 
 # Define the root dir for the task runs
-TASK_ROOT_DIR = '../Custom_runs/20250604_RES5_DCCEEW_RUNS' # Do not include the trailing slash (/) in the end of the path
+TASK_ROOT_DIR = '../Custom_runs/20250610_RES13_TEST_BETA' # Do not include the trailing slash (/) in the end of the path
 
 
 # Set the grid search parameters
@@ -36,7 +36,7 @@ grid_search = {
     # Task run settings for submitting the job to the cluster
     ###############################################################
     'MEM': ['20GB'],
-    'NCPUS':[10],
+    'NCPUS':[5],
     'TIME': ['4:00:00'],
     'QUEUE': ['normalsr'],
     
@@ -45,11 +45,11 @@ grid_search = {
     # Working settings for the model run
     ###############################################################
     'OBJECTIVE': ['maxprofit'],                 # 'maxprofit' or 'mincost'
-    'RESFACTOR': [3],
+    'RESFACTOR': [13],
     'SIM_YEARS': [list(range(2020,2051,5))],   # Years to run the model 
-    'WRITE_THREADS': [2],
-    'WRITE_OUTPUT_GEOTIFFS': [True],
-    'KEEP_OUTPUTS': [True],                    # If False, only keep report HTML
+    'WRITE_THREADS': [10],
+    'WRITE_OUTPUT_GEOTIFFS': [False],
+    'KEEP_OUTPUTS': [False],                    # If False, only keep report HTML
     
  
     ###############################################################
@@ -67,7 +67,7 @@ grid_search = {
        
     
     # --------------- GHG settings ---------------
-    'GHG_EMISSIONS_LIMITS': ['low', 'medium', 'high'],              # 'off', 'low', 'medium', 'high'
+    'GHG_EMISSIONS_LIMITS': ['medium'],              # 'off', 'low', 'medium', 'high'
     'CARBON_PRICES_FIELD': ['CONSTANT'],
     'GHG_CONSTRAINT_TYPE': ['hard'],                                # 'hard' or 'soft'
     'USE_GHG_SCOPE_1': [True],                                      # True or False
@@ -76,6 +76,7 @@ grid_search = {
     # --------------- Water constraints ---------------
     'WATER_LIMITS': ['on'],                                         # 'on' or 'off'
     'WATER_CONSTRAINT_TYPE': ['hard'],                              # 'hard' or 'soft'
+    'WATER_PENALTY': [1e-5],
     'INCLUDE_WATER_LICENSE_COSTS': [1],
     
     # --------------- Biodiversity overall ---------------
@@ -84,7 +85,7 @@ grid_search = {
     'GBF2_PRIORITY_DEGRADED_AREAS_PERCENTAGE_CUT': [40],
     
     # --------------- Biodiversity settings - GBF 2 ---------------
-    'BIODIVERSTIY_TARGET_GBF_2': ['low', 'medium', 'high'],         # 'off', 'low', 'medium', 'high'
+    'BIODIVERSTIY_TARGET_GBF_2': ['medium'],         # 'off', 'low', 'medium', 'high'
     'GBF2_CONSTRAINT_TYPE': ['hard'],           # 'hard' or 'soft'
 
     # --------------- Biodiversity settings - GBF 3 ---------------
@@ -102,7 +103,7 @@ grid_search = {
     # Scenario settings for the model run
     ###############################################################
     'SOLVE_WEIGHT_ALPHA': [1],                  # between 0 and 1, if 1 will turn off biodiversity objective, if 0 will turn off profit objective
-    'SOLVE_WEIGHT_BETA': [0.9],         
+    'SOLVE_WEIGHT_BETA': np.arange(0.999,1,0.00005),         
     
     
     #-------------------- Diet BAU --------------------
@@ -130,8 +131,8 @@ if __name__ == '__main__':
     grid_search_settings_df = get_grid_search_settings_df(TASK_ROOT_DIR, default_settings_df, grid_search_param_df)
 
     # # 1) Submit task to a single linux machine, and run simulations parallely
-    create_task_runs(TASK_ROOT_DIR, grid_search_settings_df, mode='single', n_workers=min(len(grid_search_param_df), 100))
+    # create_task_runs(TASK_ROOT_DIR, grid_search_settings_df, mode='single', n_workers=min(len(grid_search_param_df), 100))
 
     # 2) Submit task to multiple linux computation nodes
-    # create_task_runs(TASK_ROOT_DIR, grid_search_settings_df, mode='cluster', max_concurrent_tasks = 500)
+    create_task_runs(TASK_ROOT_DIR, grid_search_settings_df, mode='cluster', max_concurrent_tasks = 500)
 
--- luto/tools/create_task_runs/supersede/20250527_report_grid_search_BETA_trans_costs.py
+++ luto/tools/create_task_runs/supersede/20250527_report_grid_search_BETA_trans_costs.py
@@ -31,7 +31,6 @@ p9.options.dpi = 300
 
 # Get the data
 task_root_dir = TASK_ROOT_DIR.rstrip('/')       # Or replace with the desired task run root dir
-task_root_dir = 'N:/LUF-Modelling/LUTO2_JZ/Custom_runs/20250530_GRIDSEARCH_ALPHAT_BETA'
 report_data = process_task_root_dirs(task_root_dir)
 
 print(report_data['Type'].unique())
@@ -44,7 +43,7 @@ print(report_data['Type'].unique())
 demand_df = report_data.query('Type == "Production_deviation_pct"').copy()
 
 df_demand_avg = demand_df.eval('val = abs(val)'
-    ).groupby(['GHG_EMISSIONS_LIMITS', 'BIODIVERSTIY_TARGET_GBF_2', 'SOLVE_WEIGHT_BETA']
+    ).groupby(['SOLVE_WEIGHT_BETA']
     )[['val','run_idx']].agg(val=('val', 'mean'), run_idx=('run_idx', 'first')
     ).reset_index()
 
@@ -61,7 +60,7 @@ plot_landscape_profit = (
     ) +
     p9.geom_point(size=3,stroke=0) +
     p9.theme_bw() +
-    p9.facet_grid('GHG_EMISSIONS_LIMITS~BIODIVERSTIY_TARGET_GBF_2') +
+    # p9.facet_grid('GHG_EMISSIONS_LIMITS~BIODIVERSTIY_TARGET_GBF_2') +
     p9.theme(
         strip_text=p9.element_text(size=8), 
         legend_position='bottom',
@@ -73,8 +72,6 @@ plot_landscape_profit = (
 # Plot individual demand landscape 
 query_str = '''
     year == 2050 
-    and GHG_EMISSIONS_LIMITS == "medium" 
-    and BIODIVERSTIY_TARGET_GBF_2 == "medium"
     and abs(val) < 30
     '''.replace('\n', ' ').replace('  ', ' ')
     
@@ -89,7 +86,7 @@ plot_landscape_profit = (
         )
     ) +
     p9.geom_point(size=0.1) +
-    p9.geom_vline(xintercept=0.9,color='red') +
+    p9.geom_vline(xintercept=0.9993,color='red') +
     p9.theme_bw() +
     p9.facet_wrap('name', scales='free_y') +
     p9.theme(
