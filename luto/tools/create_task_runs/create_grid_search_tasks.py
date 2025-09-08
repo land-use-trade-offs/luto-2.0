1f47c4386954c70e227d8fd080e2fdad32bdf8f1
Jinzhu WANG <wangjinzhulala@gmail.com>
2025-09-08 11:20:27 +1000

    chore: update task run parameters

--- luto/tools/create_task_runs/create_grid_search_tasks.py
+++ luto/tools/create_task_runs/create_grid_search_tasks.py
@@ -27,7 +27,7 @@ from luto.tools.create_task_runs.helpers import (
 )
 
 # Define the root dir for the task runs
-TASK_ROOT_DIR = '../Custom_runs/20250902_RES3_NON_AG_2_5_10_15_20_25_30' # Do not include the trailing slash (/) in the end of the path
+TASK_ROOT_DIR = '../Custom_runs/20250904_RES1_HIGH_HIGH_NON_AG_15' # Do not include the trailing slash (/) in the end of the path
 
 
 # Set the grid search parameters
@@ -35,9 +35,9 @@ grid_search = {
     ###############################################################
     # Task run settings for submitting the job to the cluster
     ###############################################################
-    'MEM': ['96GB'],
-    'NCPUS':[24],
-    'TIME': ['12:00:00'],
+    'MEM': ['400GB'],
+    'NCPUS':[100],
+    'TIME': ['48:00:00'],
     'QUEUE': ['normalsr'],                                                  # normalsr for CPU, hugemembw for memory intensive jobs
     
  
@@ -45,8 +45,8 @@ grid_search = {
     # Working settings for the model run
     ###############################################################
     'OBJECTIVE': ['maxprofit'],                                             # 'maxprofit' or 'mincost'
-    'RESFACTOR': [3],
-    'SIM_YEARS': [range(2020, 2051, 5)],                                    # Years to run the model 
+    'RESFACTOR': [1],
+    'SIM_YEARS': [[2020, 2050], [2050]],                                    # Years to run the model 
     'WRITE_THREADS': [2],
     
  
@@ -68,13 +68,13 @@ grid_search = {
 
     # --------------- Social license ---------------
     'EXCLUDE_NO_GO_LU': [False],                                            # True or False
-    'REGIONAL_ADOPTION_CONSTRAINTS': ['off', 'NON_AG_UNIFORM'],             # 'off', 'on', 'NON_AG_UNIFORM'    
-    'REGIONAL_ADOPTION_NON_AG_UNIFORM': [2, 5, 10, 15, 20, 25, 30],         # Only work under 'NON_AG_UNIFORM'; None or numbers between 0-100 (both inclusive);  E.g., 5 means each non-ag land can not exceed 5% adoption in every region
-    'REGIONAL_ADOPTION_ZONE': ['NRM_CODE', 'NRM_CODE'],                     # One of 'ABARES_AAGIS', 'LGA_CODE', 'NRM_CODE', 'IBRA_ID', 'SLA_5DIGIT'
+    'REGIONAL_ADOPTION_CONSTRAINTS': ['NON_AG_UNIFORM'],                    # 'off', 'on', 'NON_AG_UNIFORM'    
+    'REGIONAL_ADOPTION_NON_AG_UNIFORM': [15],                               # Only work under 'NON_AG_UNIFORM'; None or numbers between 0-100 (both inclusive);  E.g., 5 means each non-ag land can not exceed 5% adoption in every region
+    'REGIONAL_ADOPTION_ZONE': ['NRM_CODE'],                                 # One of 'ABARES_AAGIS', 'LGA_CODE', 'NRM_CODE', 'IBRA_ID', 'SLA_5DIGIT'
 
 
     # --------------- GHG settings ---------------
-    'GHG_EMISSIONS_LIMITS': ['low', 'high'],                                # 'off', 'low', 'medium', 'high'
+    'GHG_EMISSIONS_LIMITS': ['high'],                                       # 'off', 'low', 'medium', 'high'
     'CARBON_PRICES_FIELD': ['CONSTANT'],
     'GHG_CONSTRAINT_TYPE': ['hard'],                                        # 'hard' or 'soft'
     'USE_GHG_SCOPE_1': [True],                                              # True or False
@@ -90,7 +90,7 @@ grid_search = {
     # --------------- Biodiversity overall ---------------
     'HABITAT_CONDITION': ['USER_DEFINED'],                                  # One of [10, 25, 50, 75, 90], or 'USER_DEFINED'              
     'CONNECTIVITY_SOURCE': ['NCI'],
-    'GBF2_PRIORITY_DEGRADED_AREAS_PERCENTAGE_CUT': [20, 40],                # Percentage of degraded areas to cut in GBF2 priority areas
+    'GBF2_PRIORITY_DEGRADED_AREAS_PERCENTAGE_CUT': [40],                # Percentage of degraded areas to cut in GBF2 priority areas
     
     # --------------- Biodiversity settings - GBF 2 ---------------
     'BIODIVERSITY_TARGET_GBF_2': ['high'],                                  # 'off', 'low', 'medium', 'high'
